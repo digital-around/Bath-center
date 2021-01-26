0715865262a7edaef856cbbf5bfe7ded8b634649
isakov-dev <isakov.developer@gmail.com>
2021-01-26 18:12:29 +0600

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    gcmq = require('gulp-group-css-media-queries'),
+    fileinclude = require('gulp-file-include'),
+    htmlbeautify = require('gulp-html-beautify');
+
+let path = {
+    build: {
+        css: 'css/',
+        html: './',
+    },
+    src: {
+        css: 'src/scss/style.scss',
+        html: 'src/html/pages/*.html',
+    },
+    watch: {
+        css: 'src/scss/**/*.scss',
+        html: 'src/html/**/*.html',
+        data: 'data/**/*.json',
+    }
+};
+
+function styles() {
+    return gulp.src(path.src.css)
+        .pipe(sass({outputStyle: 'nested'}).on('error', sass.logError))
+        .pipe(gcmq())
+        .pipe(gulp.dest(path.build.css))
+}
+
+function html() {
+    return gulp.src(path.src.html)
+        .pipe(fileinclude())
+        .pipe(htmlbeautify({indentSize: 4}))
+        .pipe(gulp.dest(path.build.html));
+}
+
+function watch() {
+    gulp.watch(path.watch.css, styles)
+    gulp.watch(path.watch.html, html)
+    gulp.watch(path.watch.data, html)
+}
+
+exports.default = gulp.parallel(html, styles, watch);
