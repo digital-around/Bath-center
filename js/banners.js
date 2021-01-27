42dc892099befc463aa5789dff8e2182b0260806
isakov-dev <isakov.developer@gmail.com>
2021-01-27 15:58:29 +0600

    Added fonts & desktop banners

---
+++ js/banners.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+
+    $('.banners').flickity({
+        prevNextButtons: false,
+        pageDots: true,
+        fade: true,
+        autoPlay: 5000,
+        wrapAround: true,
+    });
+
+});
