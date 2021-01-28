c80ad9170ed244fc177ffaa17d41dca40392fcf7
isakov-dev <isakov.developer@gmail.com>
2021-01-28 16:07:15 +0600

    Completed products carousel & product card, added offer block & advantages, changed fonts

---
+++ js/products-carousel.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $('.products-carousel__carousel').flickity({
+        prevNextButtons: false,
+        pageDots: false,
+        autoPlay: 5000,
+        wrapAround: true,
+    });
+
+});
