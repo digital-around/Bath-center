8ae1b952f2c2b3d2ce37aa373c0ec30ea5dd4c1a
isakov-dev <isakov.developer@gmail.com>
2021-02-03 15:03:58 +0600

    Completed catalog menu with JS behavior

---
+++ js/catalog-menu.js
@@ -0,0 +1,42 @@
+let $catalogMenu;
+let catalogMenuInit = false;
+let $categoryItem;
+let activeSubcategory;
+
+$(document).ready(function () {
+
+    $catalogMenu = $('.catalog-menu');
+    let $categoryLink = $('.catalog-menu__category');
+    $categoryItem = $('.catalog-menu__right');
+    activeSubcategory = $categoryLink.first().attr('data-subcategory');
+    let timer;
+
+    $categoryLink.mouseenter(function() {
+        activeSubcategory = $(this).attr('data-subcategory');
+        clearTimeout(timer);
+        timer = setTimeout(showSubcategory, 500, activeSubcategory);
+    });
+
+});
+
+function toggleCatalogMenu() {
+    $catalogMenu.toggleClass('catalog-menu_active');
+    if (!catalogMenuInit) {
+        showSubcategory(activeSubcategory);
+        catalogMenuInit = true;
+    }
+}
+
+function showSubcategory(id) {
+
+    $categoryItem.removeClass('catalog-menu__right_active');
+    $('.catalog-menu__right[data-subcategory="'+id+'"]').addClass('catalog-menu__right_active');
+
+    $('.catalog-menu__right_active .manufacturers-small__carousel').flickity({
+        prevNextButtons: true,
+        pageDots: false,
+        cellAlign: 'left',
+        contain: true,
+    });
+
+}
