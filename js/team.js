e0091a63af815358493dc7dbf083c59f95d6f398
isakov-dev <isakov.developer@gmail.com>
2021-03-02 18:32:18 +0600

    Added about & team blocks on about page

---
+++ js/team.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+    $('.team__carousel').flickity({
+        prevNextButtons: false,
+        pageDots: false,
+        autoPlay: 5000,
+        wrapAround: true,
+    });
+
+});
